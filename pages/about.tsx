939ade4b41f6b7bd0c331164eaa7cee16fcb93b6
princesodhi <princesodhi@ymail.com>
2019-03-15 06:19:41 +0100

    add about and contact page

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../src/Layout';
+
+const About = props => (
+  <Layout>
+    <div>About page</div>
+  </Layout>
+);
+
+export default About;
---
+++ pages/contact.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../src/Layout';
+
+const Contact = props => (
+  <Layout>
+    <div>Contact page</div>
+  </Layout>
+);
+
+export default Contact;
